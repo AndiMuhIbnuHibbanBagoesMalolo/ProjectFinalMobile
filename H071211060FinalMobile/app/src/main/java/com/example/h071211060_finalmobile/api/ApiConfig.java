4194596fd57adcf58ca63d6bc5573f1ef469f4fb
Andi Muh. Ibnu Hibban Bagoes Malolo <andibagoes1412@gmail.com>
2023-06-11 20:06:01 +0800

    update api

---
+++ H071211060FinalMobile/app/src/main/java/com/example/h071211060_finalmobile/api/ApiConfig.java
@@ -0,0 +1,6 @@
+package com.example.h071211060_finalmobile.api;
+
+public class ApiConfig {
+    public static final String URL_BASE = "https://api.themoviedb.org/";
+    public static final String API_ENDPOINT = URL_BASE;
+}
